4e569d9f2aee283db5895ca4c55f07d504b05c34
airjlee <airjlee04@gmail.com>
2024-07-28 15:17:15 -0700

    finished parsing

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,13 @@
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
+from datetime import datetime
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import pandas as pd
 
 base_url = 'https://finviz.com/quote.ashx?t='
 tickers = ['AMZN', 'AMD', 'FB']
 news_tables = {}
+
 for ticker in tickers:
     url = base_url + ticker
     req = Request(url=url, headers={'User-Agent': 'my-app'})
@@ -13,4 +17,25 @@ for ticker in tickers:
     news_tables[ticker] = news_table
     break
 
-print(news_tables)
+parsed_data = []
+for ticker, news_table in news_tables.items():
+    if news_table:  # Check if news_table is not None
+        for row in news_table.findAll('tr'):
+            link = row.find('a')
+            date_td = row.find('td')
+            if link and date_td:
+                title = link.text
+                date_data = date_td.text.strip().split(' ')
+
+                if len(date_data) == 1:
+                    date = ''
+                    time = date_data[0]
+                else:
+                    date = date_data[0]
+                    if date == "Today":
+                        date = datetime.today().strftime('%b-%d-%y')
+                    time = date_data[1]
+
+                parsed_data.append([ticker, date, time, title])
+
+df = pd.DataFrame(parsed_data, columns=['Ticker', 'Date', 'Time', 'Title'])
