69d1e2e8cf1dce057cd4df72608447b03e5c6632
airjlee <airjlee04@gmail.com>
2024-07-28 14:52:45 -0700

    bs find news table element

--- backend/main.py
+++ backend/main.py
@@ -3,11 +3,14 @@ from bs4 import BeautifulSoup
 
 base_url = 'https://finviz.com/quote.ashx?t='
 tickers = ['AMZN', 'AMD', 'FB']
+news_tables = {}
 for ticker in tickers:
     url = base_url + ticker
     req = Request(url=url, headers={'User-Agent': 'my-app'})
     response = urlopen(req)
-    print(response)
-
-    html = BeautifulSoup(response, 'html')
+    html = BeautifulSoup(response, 'html.parser')
+    news_table = html.find(id="news-table")
+    news_tables[ticker] = news_table
     break
+
+print(news_tables)
